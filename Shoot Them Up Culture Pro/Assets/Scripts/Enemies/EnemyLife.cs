497dc25a6ab2d25e8f4a93749be42af8639f5472
Loann <courcoul66@gmail.com>
2021-02-10 16:32:15 +0100

    Push EnemyLife

---
+++ Shoot Them Up Culture Pro/Assets/Scripts/Enemies/EnemyLife.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLife : MonoBehaviour
+{
+    public int life;
+    
+    public void Damage(int amount)
+    {
+        life -= amount;
+        if (life <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
