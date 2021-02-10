3788eaf4d66401d24f0537d74aa473ca1351e11b
sam <samuel.brb19@gmail.com>
2021-02-10 16:55:23 +0100

    spawnEnemi

---
+++ Shoot Them Up Culture Pro/Assets/Scripts/SpawnerEnemis.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerEnemis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemi",0,Random.Range(.8f,1.5f));
+    }
+
+    void SpawnEnemi()
+    {
+        Vector2  _screenBounds =
+            Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        //TODO faire spawn de l'objectPooler
+        //ObjectPooler.instance.SpawnFromPool("Enemi", new Vector2(Random.Range(_screenBounds.x*-1,_screenBounds.x),_screenBounds.y*-1), Quaternion.identity);
+    }
+}
