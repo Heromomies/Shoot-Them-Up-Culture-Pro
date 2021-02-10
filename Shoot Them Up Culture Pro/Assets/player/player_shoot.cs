83ffc847208a2b3552637f04dba1d7bf6a48ebca
Timothé <rondeautimothe@gmail.com>
2021-02-10 16:55:35 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Shoot Them Up Culture Pro/Assets/player/player_shoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_shoot : MonoBehaviour
+{
+    private poolManager _poolManager;
+    public Transform shootpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _poolManager = poolManager.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _poolManager.SpawnFromPool("Bullet", shootpoint.position, Quaternion.identity);
+
+        }
+    }
+}
---
+++ Shoot Them Up Culture Pro/Assets/player/poolManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poolManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionnary;
+
+    public static poolManager instance;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        poolDictionnary = new Dictionary<string, Queue<GameObject>>();
+
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+
+            for (int i = 0; i < pool.size; i++)
+            {
+              GameObject obj =  Instantiate(pool.prefab);
+              obj.SetActive(false);
+              objectPool.Enqueue(obj);
+            }
+            
+            
+            poolDictionnary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+
+        if (poolDictionnary.ContainsKey(tag))
+        {
+            return null;
+        }
+        
+        
+     GameObject objectToSpawn =   poolDictionnary[tag].Dequeue();
+     
+     
+     objectToSpawn.SetActive(true);
+     objectToSpawn.transform.position = position;
+     objectToSpawn.transform.rotation = rotation;
+     
+     poolDictionnary[tag].Enqueue(objectToSpawn);
+
+     return objectToSpawn;
+    }
+
+  
+}
