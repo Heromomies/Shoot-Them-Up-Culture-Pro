b5170ce4c1b457e082e53cd4fec92547d691b6b1
Loann <courcoul66@gmail.com>
2021-02-10 16:48:56 +0100

    Finish enemy

---
+++ Shoot Them Up Culture Pro/Assets/Scripts/Enemies/EnemyMovementThree.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementThree : MonoBehaviour
+{
+
+    public float speed;
+    public float stoppingDistance;
+    
+    private Transform _target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, _target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _target.position, speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Shoot Them Up Culture Pro/Assets/Scripts/Enemies/EnemyMovementTwo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementTwo : MonoBehaviour
+{
+    public float speed;
+
+    public float frequency, magnitude;
+
+    private Vector3 _pos;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        _pos -= transform.up * (Time.deltaTime * speed);
+        transform.position = _pos + transform.right * (Mathf.Cos(Time.time * frequency) * magnitude);
+    }
+}
