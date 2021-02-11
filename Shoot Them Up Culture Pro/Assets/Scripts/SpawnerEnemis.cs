f2407bbc6c49567ff52d19d940095f438fef8c78
sam <samuel.brb19@gmail.com>
2021-02-11 17:16:14 +0100

    c'est bon il y a les ennemis bouffon

--- Shoot Them Up Culture Pro/Assets/Scripts/SpawnerEnemis.cs
+++ Shoot Them Up Culture Pro/Assets/Scripts/SpawnerEnemis.cs
@@ -12,9 +12,7 @@ public class SpawnerEnemis : MonoBehaviour
 
     void SpawnEnemi()
     {
-        Vector2  _screenBounds =
-            Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        //TODO faire spawn de l'objectPooler
-        poolManager.instance.SpawnFromPool("Enemi", new Vector2(Random.Range(_screenBounds.x*-1,_screenBounds.x),_screenBounds.y*-1), Quaternion.identity);
+        Vector2  _screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        poolManager.instance.SpawnFromPool("Enemi", new Vector2(Random.Range(_screenBounds.x*-1,_screenBounds.x),_screenBounds.y), Quaternion.identity);
     }
 }
