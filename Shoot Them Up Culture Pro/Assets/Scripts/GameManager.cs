72918f1ec55645255dbdca486eebb1c9c20770ba
sam <samuel.brb19@gmail.com>
2021-02-11 16:46:10 +0100

    public score

--- Shoot Them Up Culture Pro/Assets/Scripts/GameManager.cs
+++ Shoot Them Up Culture Pro/Assets/Scripts/GameManager.cs
@@ -1,12 +1,21 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreTxt;
     public TextMeshProUGUI lifeTxt;
-    private int _score;
+    
+    public int score;
+    
     private int _life;
+    
+    public GameObject panelGameOver;
+    public GameObject panelPause;
+
+    private bool _isPause;
     public static GameManager instance;
 
     private void Awake()
@@ -14,10 +23,32 @@ public class GameManager : MonoBehaviour
         instance = this;
     }
 
+    private void Update()
+    {
+        if (_life <= 0)
+        {
+            Time.timeScale = 0;
+            panelGameOver.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && _life > 0 && !_isPause)
+        {
+            _isPause = true;
+            Time.timeScale = 0;
+            panelPause.SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && _life > 0 && _isPause)
+        {
+            _isPause = false;
+            Time.timeScale = 1;
+            panelPause.SetActive(false);
+        }
+    }
+
     public void UpdateScore(int scoreToAdd)
     {
-        _score += scoreToAdd;
-        scoreTxt.text = $"Score : {_score}";
+        score += scoreToAdd;
+        scoreTxt.text = $"Score : {score}";
     }
 
     public void UpdateLife(int lifeToAdd)
