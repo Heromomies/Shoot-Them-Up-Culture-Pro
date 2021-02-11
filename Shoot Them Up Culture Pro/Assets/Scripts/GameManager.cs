2db1f9e01c38d1c5e7a0bc310c4556c2e46244b8
sam <samuel.brb19@gmail.com>
2021-02-11 16:23:17 +0100

    instance GM plus canvas sur ma scène

---
+++ Shoot Them Up Culture Pro/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreTxt;
+    public TextMeshProUGUI lifeTxt;
+    private int _score;
+    private int _life;
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        _score += scoreToAdd;
+        scoreTxt.text = $"Score : {_score}";
+    }
+
+    public void UpdateLife(int lifeToAdd)
+    {
+        _life += lifeToAdd;
+        lifeTxt.text = $"Life : {_life}";
+    }
+}
