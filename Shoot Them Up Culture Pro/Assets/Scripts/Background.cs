fc7286a105af2d72db5a3a8e78f9e32726e4f243
Timothé <rondeautimothe@gmail.com>
2021-04-01 16:49:50 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ Shoot Them Up Culture Pro/Assets/Scripts/Background.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Background : MonoBehaviour
+{
+    Material material;
+    Vector2 offset;
+    public float xVelocity = 0f;
+    public float yVelocity = 0f;
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector2(xVelocity, yVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = new Vector2(xVelocity, yVelocity);
+        material.mainTextureOffset += offset * Time.deltaTime;
+
+    }
+}
