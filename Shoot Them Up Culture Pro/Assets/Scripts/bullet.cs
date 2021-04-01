37237e3663bf5721a3d9a678d186e8109a119d38
Loann <courcoul66@gmail.com>
2021-04-01 16:48:35 +0200

    Push sprite

--- Shoot Them Up Culture Pro/Assets/Scripts/bullet.cs
+++ Shoot Them Up Culture Pro/Assets/Scripts/bullet.cs
@@ -5,27 +5,18 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
-   
-
     public float speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
         transform.position += transform.up * (speed * Time.deltaTime);
-        
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.GetComponent<EnemyLife>())
         {
-            Debug.Log("ui");
             other.gameObject.GetComponent<EnemyLife>().Damage(1);
         }
 
